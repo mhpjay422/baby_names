c9a4324b1b38927cd74296fc5297a45fd0d9ea9e
Jason Gong <jasongong83@gmail.com>
2021-04-07 13:47:16 -0400

    Complete: Tasks 4-7: add where class method that is able to search for a target column type with corresponding value to find in the csv

--- name.rb
+++ name.rb
@@ -25,9 +25,20 @@ class Name
   def self.find_by_year(year)
     rows = CSV.read(@@filename, headers: true)
     result = []
-    rows.each {|row| 
-      # binding.pry
-      result << Name.new(row) if row["Year of Birth"] == year.to_s}
+    rows.each {|row| result << Name.new(row) if row["Year of Birth"] == year.to_s}
+    result
+  end
+
+  def self.where(details)
+    rows = CSV.read(@@filename, headers: true)
+    result = []
+    format_details_to_csv = details.keys[0].to_s.capitalize
+    if details.keys[0].to_s.to_i == 0
+      detail_value = details.values[0].upcase
+    else
+      detail_value = details.values[0]
+    end
+    rows.each {|row| result << Name.new(row) if row[format_details_to_csv] == detail_value}
     result
   end
 end
@@ -40,3 +51,8 @@ puts Name.find_by_name("Omar").length
 puts Name.find_by_name("Riley").length 
 puts Name.find_by_name("HUNTER").length
 puts Name.find_by_year(2011).length
+puts Name.where({ rank: "15" }).length
+puts Name.where({ rank: "25" }).length
+puts Name.where({ gender: "male" }).length
+puts Name.where({ gender: "female" }).length
+puts Name.where({ Ethnicity: "BLACK NON HISPANIC" }).length
